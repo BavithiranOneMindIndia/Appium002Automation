19f2d58f2a8c4edcad1106653b0041702fbab66f
BavithiranOneMindIndia <bavithiran@onemindindia.com>
2020-10-17 12:13:02 +0530

    First Commit 1.01

---
+++ src/main/java/AppiumPackage002GroupId/App.java
@@ -0,0 +1,13 @@
+package AppiumPackage002GroupId;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
